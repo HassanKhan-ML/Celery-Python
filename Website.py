dcc3b3c3f1eebecaa56ee491b0bc23725f5d92b6
hassan jamal khan <hassan.jamal@mobylogix.com>
2022-02-08 18:12:49 +0500

    Working on celery

---
+++ Website.py
@@ -0,0 +1,11 @@
+
+
+# website = [
+
+#   'https://linkedcamp.com/',
+#   '',
+#   '',
+# ]
+
+for i in range(12):
+  print(i)
---
+++ tasks.py
@@ -0,0 +1,28 @@
+# 1.Celery+Redis+Sqlite.py
+
+# from celery import Celery
+
+# app = Celery('hello',broker='redis://localhost:6379/0')
+
+# @app.task
+# def hello():
+#     return 'hello world'
+
+
+from celery import Celery
+from time import sleep
+
+# app = Celery('tasks', broker='redis://localhost:6379/0')
+app = Celery('tasks', backend= "db+sqlite:///db.sqlite3"
+)
+
+app.conf.broker_url = 'redis://localhost:6379/0'
+# app.conf.result_backend = 'redis://localhost:6379/0'
+# app.conf.broker_url = 'redis://localhost:6379/0'
+
+@app.task
+def add(x):
+  sleep(2)
+  for i in range(x):
+    print(i)
+  return x 
