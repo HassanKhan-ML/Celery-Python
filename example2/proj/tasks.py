c0a79ee84b1dd0ca53ffe05aacf2b22c6451ebf8
hassan jamal khan <hassan.jamal@mobylogix.com>
2022-02-15 18:58:03 +0500

    Working on flower

--- example2/proj/tasks.py
+++ example2/proj/tasks.py
@@ -15,3 +15,6 @@ def mul(x, y):
 @app.task
 def xsum(numbers):
     return sum(numbers)
+
+
+# $ celery flower --auth="me@gmail.com|you@gmail.com" --oauth2_key=631896940196-73ee802a2cko4paci1e25n3a1jp3n1k5.apps.googleusercontent.com --oauth2_secret=TQza-0xY8VR6VX-3MLVDZmb4 --oauth2_redirect_uri=http://flower.example.com/login
